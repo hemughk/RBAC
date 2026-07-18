c857635264b87c7db61cb7577f706b02b2d2b028
hemughk <45702280+hemughk@users.noreply.github.com>
2026-07-18 17:33:17 +0530

    Add Role entity model

---
+++ src/main/java/com/example/rbac/model/Role.java
@@ -0,0 +1,29 @@
+package com.example.rbac.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    private Set<User> users = new HashSet<>();
+}
